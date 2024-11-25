12a77f3b12b2b21adb908d93aa116120c748448b
Priver <priverdeprince@yahoo.com>
2024-11-25 16:04:41 +0300

    Modified the Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,55 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion"; // Make sure this is imported
+import { motion } from "framer-motion";
+
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-  const [text, setText] = useState("");
-  const fullText = "Adrian";
-  const typingSpeed = 150; // Typing speed in milliseconds
-  const deletingSpeed = 100; // Deleting speed in milliseconds
-  const delayBeforeDelete = 1000; // Delay before starting to delete
-
-  useEffect(() => {
-    let typingInterval;
-    let deletingInterval;
-
-    const typeText = () => {
-      let index = 0;
-      typingInterval = setInterval(() => {
-        setText((prevText) => prevText + fullText[index]);
-        index += 1;
-        if (index === fullText.length) {
-          clearInterval(typingInterval);
-          setTimeout(deleteText, delayBeforeDelete); // Start deleting after a delay
-        }
-      }, typingSpeed);
-    };
-
-    const deleteText = () => {
-      let index = fullText.length;
-      deletingInterval = setInterval(() => {
-        setText((prevText) => prevText.slice(0, index - 1));
-        index -= 1;
-        if (index === 0) {
-          clearInterval(deletingInterval);
-          setTimeout(typeText, delayBeforeDelete); // Start typing again after a delay
-        }
-      }, deletingSpeed);
-    };
-
-    typeText();
-
-    return () => {
-      clearInterval(typingInterval);
-      clearInterval(deletingInterval);
-    };
-  }, []);
-
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -58,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">{text}</span>
+            Hi, I'm <span className="text-[#915EFF]">Adrian</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="sm:block hidden" />
